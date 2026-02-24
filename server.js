54fc05c50014d1781d27d096fc5558ed6ae5623d
Ndelosreyes-maker <Ndelosreyes@legendarytherapy.com>
2026-02-24 22:37:35 +0800

    Update server.js

--- server.js
+++ server.js
@@ -45,6 +45,8 @@ app.post("/upload", upload.single("file"), async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
